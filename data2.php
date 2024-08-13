4f332242725719a815f2248709fb843399f9e3ee
rynarf25 <aripsatrio45@gmail.com>
2024-08-14 05:46:10 +0700

    first commit

---
+++ data2.php
@@ -0,0 +1,78 @@
+<?php
+
+$konek = mysqli_connect("localhost", "root", "", "iot_gas");
+
+$sql_ID = mysqli_query($konek, "SELECT MAX(ID) FROM sensor");
+
+$data_ID = mysqli_fetch_array($sql_ID);
+
+$ID_akhir = $data_ID['MAX(ID)'];
+$ID_awal =  $ID_akhir - 6 ;
+
+$tanggal = mysqli_query($konek, "SELECT DATE_FORMAT(tanggal, '%H:%i:%s') AS waktu from sensor WHERE ID>='$ID_awal' and ID<='$ID_akhir' ORDER BY ID ASC");
+
+$tinggiair = mysqli_query($konek, "SELECT tinggiair from sensor WHERE ID>='$ID_awal' and ID<='$ID_akhir' ORDER BY ID ASC");
+
+
+?>
+
+<div class="card-body">
+    <canvas id="mychart2"></canvas>
+    <script type="text/javascript">
+        var canvas2 = document.getElementById('mychart2');
+        var data2 = {
+            labels : [
+                <?php
+                    while($data_tanggal = mysqli_fetch_array($tanggal))   
+                    {
+                        echo '"'.$data_tanggal['waktu'].'",';
+                    }             
+                ?>
+            ],
+            datasets : [ 
+            {
+                label : "Tinggi Air", 
+                fill : true,
+                backgroundColor : "rgba(239, 82, 93, 0.2)",
+                borderColor : "rgba(239, 82, 93, 1)",
+                lineTension : 0.5,
+                pointRadius : 5,
+                data : [
+                    <?php
+                        while($data_tinggiair = mysqli_fetch_array($tinggiair))
+                        {
+                            echo $data_tinggiair['tinggiair'].',' ;
+                        }
+                    ?>
+                ]
+            }
+        ] 
+
+        } ;
+
+        var options2 = {
+            scales: {
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Waktu' // Label untuk sumbu x
+                    }
+                },
+                y: {
+                    title: {
+                        display: true,
+                        text: 'Nilai' // Label untuk sumbu y
+                    }
+                }
+            },
+            showLines: true,
+            animation: { duration: 5 }
+        };
+
+        var myLineChart2 = new Chart(canvas2, {
+            type: 'line',
+            data: data2,
+            options: options2
+        });
+    </script>
+</div>
---
+++ kirimdata.php
@@ -0,0 +1,15 @@
+<?php
+
+    $konek = mysqli_connect("localhost", "root", "", "iot_gas");
+
+    $mq136 = $_GET["mq136"];
+    $mq135 = $_GET["mq135"];   
+    $mq4 = $GET["mq4"];
+    $tinggiair = $_GET["tinggiair"];
+    $status = $_GET["status"];
+
+    mysqli_query($konek,"ALTER TABLE sensor AUTO_INCREMENT=1");
+    mysqli_query($konek, "INSERT INTO sensor(ppm_mq136, ppm_mq135, ppm_mq4, tinggiair, status)VALUES('$mq136', '$mq135', '$tinggiair', '$status')");
+    
+
+?>
